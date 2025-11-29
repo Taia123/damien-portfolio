3a1559b800b478bd03d5504e32a1fe754284f15f
Taia123 <abuseridze.taia@gmail.com>
2025-11-29 13:29:51 +0400

    Finish About page layout with hero, biography, timeline, testimonials, and footer

--- src/app/pages/about-page/about-page.ts
+++ src/app/pages/about-page/about-page.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../components/navbar/navbar'; 
+import { TestimonialSectionComponent } from '../../components/testimonial-section/testimonial-section';
+import { FooterSectionComponent } from '../../components/footer-section/footer-section';
+
 
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
-  imports: [CommonModule, NavbarComponent],
+  imports: [CommonModule, NavbarComponent, TestimonialSectionComponent, FooterSectionComponent],
   templateUrl: './about-page.html',
   styleUrls: ['./about-page.scss'],
 })
